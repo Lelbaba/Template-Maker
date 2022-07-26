7bc9c0d79e52073f26e808a13e133eefcefb68be
lelbaba <abdullahabrar9033@gmail.com>
2022-07-27 05:38:23 +0600

    added debug template

--- Codes/All Macros.cpp
+++ Codes/All Macros.cpp
@@ -11,16 +11,33 @@ using namespace __gnu_pbds;
 template <typename DT> 
 using ordered_set = tree <DT, null_type, less<DT>, rb_tree_tag,tree_order_statistics_node_update>;
 
-int kx[] =              {-2,-2,-1,+1,+2,+2,+1,-1};
-int ky[] =              {-1,+1,+2,+2,+1,-1,-2,-2};
+// debug template
+void __print(int x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << "{"; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef SONIC
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
 
 #define fastio            ios_base::sync_with_stdio(0);cin.tie(0);
-
 #define Make(x,p)       (x | (1<<p))
 #define DeMake(x,p)     (x & ~(1<<p))
 #define Check(x,p)      (x & (1<<p))
-
-#define DEBUG(x)        cerr << #x << " = " << x << endl
 
 template<size_t N>
 bitset<N> rotl( std::bitset<N> const& bits, unsigned count ) {
